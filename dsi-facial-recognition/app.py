b0ec72f4f1b6bde5a939332d65e780fc92e57b4f
Mario Sánchez García <msanchez@advval.com>
2019-05-09 13:50:42 +0200

    using multiple haar cascades when no detected

--- dsi-facial-recognition/app.py
+++ dsi-facial-recognition/app.py
@@ -1,57 +1,95 @@
 import numpy as np
 import cv2
 import pickle
+import logging
+import coloredlogs
 
-#face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read("trainner.yml")
 
-labels = {"person_name": 1}
-with open("labels.pickle", 'rb') as f:
-    og_labels = pickle.load(f)
-    labels = {v:k for k,v in og_labels.items()}
+logger = logging.getLogger(__name__)
 
-cap = cv2.VideoCapture(0)
 
-while(True):
+def initLogger(level='DEBUG'):
+    # fmt = '%(asctime)s - %(levelname)s: %(message)s'
+    fmt = '%(asctime)s - %(message)s'
 
-    ret, frame = cap.read()
+    coloredlogs.install(fmt=fmt, datefmt='%d/%m/%Y %H:%M:%S', level=level)
+    # logger.info('Logger is active')
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+def recognise():
+    #face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+    face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read("trainner.yml")
 
-    #Region detectada
-    for (x, y, w, h) in faces:
-        # print (x, y, w, h)
+    labels = {"person_name": 1}
+    with open("labels.pickle", 'rb') as f:
+        og_labels = pickle.load(f)
+        labels = {v:k for k,v in og_labels.items()}
 
-        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, y_cord_start + height)
-        roi_color = frame[y:y+h, x:x+w] #(xcord_start, x_cord_start + width)
+    cap = cv2.VideoCapture(0)
 
-        id_, conf = recognizer.predict(roi_gray) 
-        confidence = 100 - (conf*100 / 255)
-        if confidence >= 10: #and conf <= 85:
-            # print (id_)
-            # print (labels[id_])
-            cv2.putText(frame, labels[id_] + ": " + '{:3.2f}'.format(confidence), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
-        
-        if confidence > 100:
-            print('ERROR, cnonf over 100% ({conf})'.format(conf=confidence))
+    faceCascades = []
+    for model in ['default', 'alt', 'alt2', 'alt_tree']:
+        p = 'cascades/data/haarcascade_frontalface_{model}.xml'
+        faceCascades.append(cv2.CascadeClassifier(p.format(model=model)))
 
-        # img_item = "my-image.png"
-        # cv2.imwrite(img_item, roi_gray)
+    while(True):
 
-        color = (255, 0, 0) #BGR
-        stroke = 2
-        end_cord_x = x + w
-        end_cord_y = y + h
+        ret, frame = cap.read()
+        idx = 0
 
-        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+        for face_cascade in faceCascades:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-    #Mostramos el frame resultante
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break;
+            #Region detectada
+            for (x, y, w, h) in faces:
+                # print (x, y, w, h)
 
-cap.release()
-cv2.destroyAllWindows()
+                croppedROI = gray[y:y+h, x:x+w].copy()
+                croppedROI = cv2.resize(croppedROI, (160, 160))
+
+                # roi_gray = gray[y:y+h, x:x+w] #(ycord_start, y_cord_start + height)
+                # roi_color = frame[y:y+h, x:x+w] #(xcord_start, x_cord_start + width)
+
+                id_, conf = recognizer.predict(croppedROI) 
+                confidence = 100 - (conf*100 / 255)
+                if confidence >= 60: #and conf <= 85:
+                    # print (id_)
+                    # print (labels[id_])
+                    cv2.putText(frame, labels[id_] + ": " + '{:3.2f}'.format(confidence), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+                
+                if confidence > 100:
+                    print('ERROR, cnonf over 100% ({conf})'.format(conf=confidence))
+
+                # img_item = "my-image.png"
+                # cv2.imwrite(img_item, roi_gray)
+
+                color = (255, 0, 0) #BGR
+                stroke = 2
+                end_cord_x = x + w
+                end_cord_y = y + h
+
+                cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+            if len(faces) > 0:
+                # logger.debug(path.split('\\')[-1] + '\t✓')
+                break
+            else:
+                if idx == len(faceCascades)-1:
+                    logger.warning('\t⛌ [{}]'.format(idx))
+            idx += 1
+
+        #Mostramos el frame resultante
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break;
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    initLogger()
+
+    recognise()
--- dsi-facial-recognition/face-train.py
+++ dsi-facial-recognition/face-train.py
@@ -3,62 +3,106 @@ from PIL import Image
 import numpy as np
 import cv2
 import pickle
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-image_dir = os.path.join(BASE_DIR, "images")
-
-#face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-current_id = 0
-label_ids ={}
-y_labels = []
-x_train = []
-
-for root, dirs, files in os.walk(image_dir):
-    for file in files:
-        if file.endswith("png") or file.endswith("jpg"):
-            path = os.path.join(root, file)
-            label = os.path.basename(os.path.dirname(path))
-            #print (path, label)
-            print (path[-20:])
+import logging
+import coloredlogs
 
 
-            if not label in label_ids:
-                label_ids[label] = current_id
-                current_id += 1
+logger = logging.getLogger(__name__)
+# cap = None
 
-            id_ = label_ids[label]
-            #print (label_ids)
+def initLogger(level='DEBUG'):
+    # fmt = '%(asctime)s - %(levelname)s: %(message)s'
+    fmt = '%(asctime)s - %(message)s'
 
-            #y_labels.append(label) #save the labels
-            #x_train.append(path) #save the images
+    coloredlogs.install(fmt=fmt, datefmt='%d/%m/%Y %H:%M:%S', level=level)
+    # logger.info('Logger is active')
 
-            pil_image = Image.open(path).convert("L") #grayscale
-            
-            #size = (500, 500)
-            #pil_image = pil_image.resize(size, Image.ANTIALIAS)
 
-            #image_array = np.array(final_image, "uint8")
-            image_array = np.array(pil_image, "uint8")
-            #print (image_array)
+def createTrainModel():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    image_dir = os.path.join(BASE_DIR, "images")
 
-            #obtain the region of interest
-            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
-            #print (faces)
+    faceCascades = []
+    for model in ['default', 'alt', 'alt2', 'alt_tree']:
+        p = 'cascades/data/haarcascade_frontalface_{model}.xml'
+        faceCascades.append(cv2.CascadeClassifier(p.format(model=model)))
 
-            for (x, y, w, h) in faces:
-                print ("Cara detectada para: " + label)
-                roi = image_array[y:y+h, x:x+w]
-                x_train.append(roi)
-                y_labels.append(id_)
+    #face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+    face_cascade = cv2.CascadeClassifier()
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-#print (y_labels)
-#print (x_train)
+    current_id = 0
+    label_ids ={}
+    y_labels = []
+    x_train = []
 
-with open("labels.pickle", 'wb') as f:
-    pickle.dump(label_ids, f)
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            if file.endswith("png") or file.endswith("jpg"):
+                idx = 0
 
-recognizer.train(x_train, np.array(y_labels))
-recognizer.save("trainner.yml")
+                for face_cascade in faceCascades:
+
+                    path = os.path.join(root, file)
+                    label = os.path.basename(os.path.dirname(path))
+
+                    if not label in label_ids:
+                        label_ids[label] = current_id
+                        current_id += 1
+
+                    id_ = label_ids[label]
+                    #print (label_ids)
+
+                    #y_labels.append(label) #save the labels
+                    #x_train.append(path) #save the images
+
+                    # grayscaleImg = Image.open(path).convert("L") #grayscale
+                    grayscaleImg = cv2.imread(path, 0)
+
+                    #size = (500, 500)
+                    #grayscaleImg = grayscaleImg.resize(size, Image.ANTIALIAS)
+
+                    #image_array = np.array(final_image, "uint8")
+                    # image_array = np.array(grayscaleImg, "uint8")
+                    #print (image_array)
+
+                    #obtain the region of interest
+                    faces = face_cascade.detectMultiScale(grayscaleImg, scaleFactor=1.3, minNeighbors=5)
+                    #print (faces)
+
+                    for (x, y, w, h) in faces:
+                        # print ("Cara detectada para: " + label[0].upper() + label[1:])
+                        croppedROI = grayscaleImg[y:y+h, x:x+w].copy()
+
+                        # cv2.imshow('frame', croppedROI)
+
+                        croppedROI = cv2.resize(croppedROI, (160, 160))
+                        
+                        x_train.append(croppedROI)
+                        y_labels.append(id_)
+                    
+                    if len(faces) > 0:
+                        logger.debug(path.split('\\')[-1] + '\t✓')
+                        break
+                    else:
+                        if idx == len(faceCascades)-1:
+                            logger.error(path.split('\\')[-1] + '\t⛌ [{}]'.format(idx))
+                        else:
+                            logger.warning(path.split('\\')[-1] + '\t⛌ [{}]'.format(idx))
+                    
+                    idx += 1
+
+    #print (y_labels)
+    #print (x_train)
+
+    with open("labels.pickle", 'wb') as f:
+        pickle.dump(label_ids, f)
+
+    recognizer.train(x_train, np.array(y_labels))
+    recognizer.save("trainner.yml")
+
+
+if __name__ == "__main__":
+    initLogger()
+    createTrainModel()
+
